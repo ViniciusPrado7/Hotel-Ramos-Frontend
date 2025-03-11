2792e9b96bef2d82caaa5fa0d67939972e8e4f92
ViniciusPrado7 <vinicius.pradofake@gmail.com>
2025-03-11 14:40:14 -0300

    admin

--- src/pages/MinhaConta.jsx
+++ src/pages/MinhaConta.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
-import { BtnLogin, ContainerLogin, FormLogin, InputLogin, LabelLogin, TituloLogin } from '../styles/MinhaContaStyles'
+import { BtnLogin, ContainerLogin, FormLogin, InputLogin, LabelLogin, StyledError, StyledSuccess, TituloLogin } from '../styles/MinhaContaStyles'
 import axios from 'axios';
-
-
 
 function MinhaConta() {
 
   const [emailUser, setEmailUser] = useState('');
   const [nomeUser, setNomeUser] = useState('');
+
+  const [errorCad, setErroCad] = useState('');
+  const [successCad, setSucessCad] = useState('');
+
+  const[emailAdmin, setEmailAdmin] = useState('');
+  const[nomeAdmin, setNomeAdmin] = useState('');
+
+  const [errorCadAdmin, setErroCadAdmin] = useState('');
+  const [successCadAdmin, setSucessCadAdmin] = useState('');
+
+  
 
   const handleCadastro = async (event) => {
     event.preventDefault();
@@ -16,18 +25,63 @@ function MinhaConta() {
       email_user: emailUser,
       nome_user: nomeUser,
     }
-    const resp = await axios.post("http://localhost:3001/hotel/user", dataCadastro);
-    console.log(dataCadastro);
-    if(resp.status === 200 || resp.status === 201){
-      setEmailUser('')
-      setNomeUser('')
-      alert('Cadastro concluido com sucesso.')
+    if (!emailUser || !nomeUser) {
+      setErroCad('Todos os campos precisam ser preenchidos.');
+      setSucessCad('');
     } else {
-      setEmailUser('')
-      setNomeUser('')
-      alert('Cadastro não efetuado.')
+      try {
+        const resp = await axios.post("http://localhost:3001/hotel/user", dataCadastro);
+        if (resp.status === 200 || resp.status === 201) {
+          setEmailUser('');
+          setNomeUser('');
+          setSucessCad('Cadastro concluido com sucesso.');
+          setErroCad('');
+        } else {
+          setEmailUser('');
+          setNomeUser('');
+          setErroCad('Cadastro não efetuado.');
+          setSucessCad('');
+        }
+      } catch (error) {
+        setErroCad('Erro ao cadastrar.');
+        setSucessCad('');
+      }
     }
+
   }
+
+  const handleCadastroAdmin = async (event) => {
+    event.preventDefault();
+
+    const dataCadastroAdmin = {
+      email_admin: emailAdmin,
+      nome_admin: nomeAdmin,
+    }
+    if (!emailAdmin || !nomeAdmin) {
+      setErroCadAdmin('Todos os campos precisam ser preenchidos.');
+      setSucessCadAdmin('');
+    } else {
+      try {
+        const resp = await axios.post("http://localhost:3001/hotel/admin", dataCadastroAdmin);
+        if (resp.status === 200 || resp.status === 201) {
+          setEmailAdmin('');
+          setNomeAdmin('');
+          setSucessCadAdmin('Cadastro concluido com sucesso.');
+          setErroCadAdmin('');
+        } else {
+          setEmailAdmin('');
+          setNomeAdmin('');
+          setErroCadAdmin('Cadastro não efetuado.');
+          setSucessCadAdmin('');
+        }
+      } catch (error) {
+        setErroCadAdmin('Erro ao cadastrar.');
+        setSucessCadAdmin('');
+      }
+    }
+
+  }
+
 
   return (
     <ContainerLogin>
@@ -37,12 +91,14 @@ function MinhaConta() {
         <TituloLogin> Cadastro </TituloLogin>
 
         <LabelLogin > Email: </LabelLogin>
-        <InputLogin type="text" onChange={(e)=>setEmailUser(e.target.value)}/>
+        <InputLogin type="text" value={emailUser} onChange={(e) => setEmailUser(e.target.value)} />
 
         <LabelLogin> Nome: </LabelLogin>
-        <InputLogin type="text" onChange={(e)=>setNomeUser(e.target.value)} />
+        <InputLogin type="text" value={nomeUser} onChange={(e) => setNomeUser(e.target.value)} />
 
         <BtnLogin onClick={handleCadastro}> Cadastrar </BtnLogin>
+        {errorCad && <StyledError>{errorCad}</StyledError>}
+        {successCad && <StyledSuccess>{successCad}</StyledSuccess>}
 
       </FormLogin>
 
@@ -51,10 +107,26 @@ function MinhaConta() {
         <LabelLogin> Email: </LabelLogin>
         <InputLogin type="text" />
 
-        <LabelLogin> Senha: </LabelLogin>
-        <InputLogin type="password" />
+        <LabelLogin> Nome: </LabelLogin>
+        <InputLogin type="text" />
 
         <BtnLogin> Login</BtnLogin>
+      </FormLogin>
+
+      <FormLogin>
+
+        <TituloLogin> Cadastro Admin</TituloLogin>
+
+        <LabelLogin > Email: </LabelLogin>
+        <InputLogin type="text" value={emailAdmin} onChange={(e) => setEmailAdmin(e.target.value)} />
+
+        <LabelLogin> Nome: </LabelLogin>
+        <InputLogin type="text" value={nomeAdmin} onChange={(e) => setNomeAdmin(e.target.value)} />
+
+        <BtnLogin onClick={handleCadastroAdmin}> Cadastrar </BtnLogin>
+        {errorCadAdmin && <StyledError>{errorCadAdmin}</StyledError>}
+        {successCadAdmin && <StyledSuccess>{successCadAdmin}</StyledSuccess>}
+
       </FormLogin>
 
     </ContainerLogin>
--- src/styles/MinhaContaStyles.js
+++ src/styles/MinhaContaStyles.js
@@ -6,7 +6,6 @@ flex-direction: row;
 place-content: center;
 gap: 10rem;
 margin: 7rem 0;
-
 `
 
 export const FormLogin = styled.form `
@@ -15,22 +14,17 @@ flex-direction: column;
 background: white;
 padding: 2rem;
 border-radius: 1rem;
-
-
 `
 export const TituloLogin = styled.h1 `
-
 `
 export const InputLogin = styled.input `
 padding: .5rem;
 border-radius: .6rem;
-
 `
 export const LabelLogin = styled.label `
 margin-top: 1rem;
-
-
 `
+
 export const BtnLogin = styled.button `
 margin-top: 2rem;
 padding: 1rem;
@@ -38,4 +32,14 @@ border-radius: 1rem;
 background-color: black;
 color: white;
 cursor: pointer;
+`
+export const StyledError = styled.p `
+color: red;
+font-size: .8rem;
+text-align: center;
+`
+export const StyledSuccess = styled.p `
+color: green;
+font-size: .8rem;
+text-align: center;
 `
